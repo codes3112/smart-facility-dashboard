6086ba36813ff76236dc102a12753caa2b9fd673
Sneha Arora <sneha.arora@appnovation.com>
2025-11-05 10:45:18 -0500

    chore: setup shadcn/ui(New York style) with tailwind3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,7 +8,13 @@ function App() {
 
   return (
     <>
-   <div className="text-xl p-6 text-red-600">Hello Tailwind</div>;
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+           <div className="text-xl p-6 text-red-600">Hello Tailwind</div>;
+      </div>
     </>
   )
 }
